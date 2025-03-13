b819842c2997500f8e97dfa0e897ddeadce9523c
Tsabitah59 <tsabitahsafa06@gmail.com>
2025-03-13 10:45:27 +0700

    finishing my tash, but waiting for wcu coywriting

--- js/script.js
+++ js/script.js
@@ -1,18 +1,46 @@
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".slide-left, .slide-right, .rise-up");
 
-window.addEventListener('scroll', checkBoxes);
+window.addEventListener("scroll", checkBoxes);
 checkBoxes();
 
 function checkBoxes() {
-    const triggerBottom = window.innerHeight / 5 * 4;
-    cards.forEach((box, idx) => {
-        const cardTop = box.getBoundingClientRect().top;
-
-        if (cardTop < triggerBottom) {
-            
-            box.classList.add('show')
-        } else {
-            box.classList.remove('show')
-        }
-    })
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  cards.forEach((box, index) => {
+    const slideTop = box.getBoundingClientRect().top;
+    if (slideTop < triggerBottom) {
+      if (index === 0) {
+        setTimeout(() => {
+          box.classList.add("show");
+        }, 400);
+      } else if (index === 1) {
+        setTimeout(() => {
+          box.classList.add("show");
+        }, 200);
+      } else {
+        box.classList.add("show");
+      }
+    } else {
+      box.classList.remove("show");
+    }
+  });
 }
+
+const accordian = document.getElementsByClassName("content-accordi");
+for (index = 0; index < accordian.length; index++) {
+  accordian[index].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
+
+// Swiper.js
+var BookSlider = new Swiper(".book-sliders", {
+  grabCursor: true,
+  centeredSlides: false,
+  loop: true,
+  slidesPerView: 'auto',
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
